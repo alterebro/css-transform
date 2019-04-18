767b00921ecd2ef377ebc1adfe462e3055b1ac6c
Jorge Moreno <alterebro@gmail.com>
2019-04-18 20:09:15 +0200

    Reset and Random property functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
+const randomInt = function(min, max) {
+	// Returns a random integer between @min and @max (inclusive)
+	let _max = parseInt(max);
+	let _min = parseInt(min);
+	return Math.floor(Math.random() * ((_max - _min) + 1)) + _min;
+}
+
+let a = randomInt(1, 3);
+let b = randomInt(10, 30);
+let c = randomInt(100, 500);
+console.log(a, b, c);
 
 const Data = {
 
@@ -33,6 +44,7 @@ const Data = {
         current : null
     }
 }
+
 const App = new Vue({
 
     el : '#app',
@@ -95,7 +107,53 @@ const App = new Vue({
 
             document.querySelector('#app > article').classList.remove('visible');
             return true;
-        }
+        },
+
+		// Rotate 2D
+		randomRotate2D() { this.rotate2D = randomInt(0, 360) },
+        resetRotate2D() { this.rotate2D = 0 },
+
+		// Rotate 3D
+		randomRotate3D() {
+			this.rotateX = randomInt(0, 360);
+			this.rotateY = randomInt(0, 360);
+			this.rotateZ = randomInt(0, 360);
+		},
+		resetRotate3D() { this.rotateX = this.rotateY = this.rotateZ = 0; },
+
+		// Perspective
+		randomPerspective() { this.perspective = (randomInt(3, 23) * 100) },
+		resetPerspective() { this.perspective = 700 },
+
+		// Transform Origin
+		randomTransformOrigin() {
+			this.transformOriginX = randomInt(0, 100);
+			this.transformOriginY = randomInt(0, 100);
+			this.transformOriginZ = randomInt(-250, 250);
+		},
+		resetTransformOrigin() {
+			this.transformOriginX = this.transformOriginY = 50;
+			this.transformOriginZ = 0;
+		},
+
+		// Scale
+		randomScale() { this.scale = (randomInt(5, 20) / 10) },
+		resetScale() { this.scale = 1; },
+
+		// Translate
+		randomTranslate() {
+			this.translateX = (randomInt(-10, 10) * 10);
+			this.translateY = (randomInt(-10, 10) * 10);
+		},
+		resetTranslate() { this.translateX = this.translateY = 0 },
+
+		// Skew
+		randomSkew() {
+			this.skewX = randomInt(0, 180);
+			this.skewY = randomInt(0, 180);
+		},
+		resetSkew() { this.skewX = this.skewY = 0 },
+
     }
 
 });
