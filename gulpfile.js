8355c1bce7ce78cc6328e5cf1419fd216f820d8d
Jorge Moreno <alterebro@gmail.com>
2019-04-27 16:53:56 +0200

    build app injecting everything into index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { parallel, src, dest } = require('gulp');
+const { series, parallel, src, dest } = require('gulp');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
@@ -6,6 +6,8 @@ const htmlmin = require('gulp-htmlmin');
 const replace = require('gulp-replace');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const del = require('del');
+const fs = require('fs');
 
 function styles() {
     return src('src/css/app.less')
@@ -46,4 +48,39 @@ function modules() {
     .pipe(dest('dist/js'));
 }
 
-exports.build = parallel(styles, html, scripts, copy_files, modules);
+// ...
+
+function clean_dist_before() { return del([ 'dist/**/*' ]) }
+function clean_dist_after() {
+
+    return del([
+        'dist/css/',
+        'dist/js/',
+    ]);
+}
+
+function embed_external() {
+    return src('dist/index.html')
+    .pipe(replace(
+        /<link rel="stylesheet" href="(.*?)">/g,
+        (s, filename) => {
+            let _css = fs.readFileSync(`dist/${filename}`, 'utf8');
+            return `<style>${_css}</style>`;
+        }
+    ))
+    .pipe(replace(
+        /<script src="(.*?)"><\/script>/g,
+        (s, filename) => {
+            let _js = fs.readFileSync(`dist/${filename}`, 'utf8')
+            return `<script>${_js}</script>`;
+        }
+    ))
+    .pipe(dest('dist/'));
+}
+
+exports.prebuild = series(clean_dist_before);
+exports.create = parallel(styles, html, scripts, copy_files, modules);
+exports.embed = series(embed_external);
+exports.postbuild = series(clean_dist_after);
+
+exports.build = series(clean_dist_before, parallel(styles, html, scripts, copy_files, modules), embed_external, clean_dist_after);
